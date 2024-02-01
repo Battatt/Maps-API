84284d2c96e6b629e548e915e28860d1bd82afbc
lyceum <battamaev@yandex.ru>
2024-02-01 16:24:21 +0300

    First Task

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
-
-import pygame
+import argparse
 import requests
-import os
+from visual import visual_window
 
-# Здесб должна быть обработка через argparse
-spn = [0.5, 0.5]
-coords = [46.308014, 44.269656][::-1]
 
-maps_server = 'http://static-maps.yandex.ru/1.x/'
-map_params = {
-    'll': str(coords[0]) + ',' + str(coords[1]),
-    'spn': str(spn[0]) + ',' + str(spn[1]),
-    'l': 'map'}
-response = requests.get(maps_server, params=map_params)
-with open('map.png', 'wb') as f:
-    f.write(response.content)
-pic = pygame.image.load('map.png')
-os.remove('map.png')
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-running = True
-now = 0
-while running:
-    screen.blit(pic, (0, 0))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.flip()
-pygame.quit()
+def main():
+    spn = [0.5, 0.5]
+    coords = [46.308014, 44.269656][::-1]
+    search_params = {'ll': str(coords[0]) + ',' + str(coords[1]),
+                     'spn': str(spn[0]) + ',' + str(spn[1]),
+                     'l': 'map'}
+    api_adress = "http://static-maps.yandex.ru/1.x/"
+    visual_window(api_adress, search_params)
+
+
+if __name__ == '__main__':
+    main()
--- visual.py
+++ visual.py
@@ -4,34 +4,35 @@ import os
 import sys
 
 
-def visual_window(request):
-    response = requests.get(request)
+def visual_window(api_adress, search_params):
+    print(api_adress)
+    print(search_params)
+    response = requests.get(api_adress, params=search_params)
 
     if not response:
         print("Ошибка выполнения запроса:")
-        print(request)
         print("Http статус:", response.status_code, "(", response.reason, ")")
         sys.exit(1)
 
-    # Запишем полученное изображение в файл.
     map_file = "map.png"
     with open(map_file, "wb") as file:
         file.write(response.content)
 
-    # Инициализируем pygame
     pygame.init()
     screen = pygame.display.set_mode((600, 450))
-    # Рисуем картинку, загружаемую из только что созданного файла.
     screen.blit(pygame.image.load(map_file), (0, 0))
-    # Переключаем экран и ждем закрытия окна.
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
     pygame.quit()
-
-    # Удаляем за собой файл с изображением.
     os.remove(map_file)
 
 
 if __name__ == '__main__':
-    visual_window("http://static-maps.yandex.ru/1.x/?ll=44.301532,46.307033&spn=0.01,0.01&l=map")
+    spn = [0.5, 0.5]
+    coords = [46.308014, 44.269656][::-1]
+    search_params = {'ll': str(coords[0]) + ',' + str(coords[1]),
+                     'spn': str(spn[0]) + ',' + str(spn[1]),
+                     'l': 'map'}
+    api_adress = "http://static-maps.yandex.ru/1.x/"
+    visual_window(api_adress, search_params)
